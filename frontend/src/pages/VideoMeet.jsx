459b86edf8f1ea34b93984d6e992df370f7bf120
Ashra Ansari <ashraansari02@gmail.com>
2026-09-21 15:45:05 +0530

    Fix navigation after call

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -12,7 +12,7 @@ import ScreenShareIcon from "@mui/icons-material/ScreenShare";
 import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
 import ChatIcon from "@mui/icons-material/Chat";
 import server from "../env";
-
+import { useNavigate } from "react-router-dom";
 const server_url = server;
 
 var connections = {};
@@ -22,6 +22,7 @@ const peerConfigConnections = {
 };
 
 export default function VideoMeetComponent() {
+  const navigate = useNavigate();
   var socketRef = useRef();
   let socketIdRef = useRef();
 
@@ -459,7 +460,7 @@ export default function VideoMeetComponent() {
       let tracks = localVideoref.current.srcObject.getTracks();
       tracks.forEach((track) => track.stop());
     } catch (e) {}
-    window.location.href = "/home";
+    navigate("/home");
   };
 
   let openChat = () => {
