b9a46de5ec083603ace8490ce2adcdaccfb41ce0
Ashra Ansari <ashraansari02@gmail.com>
2026-09-21 15:18:58 +0530

    Fix backend URL

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://voxmeet-0g91.onrender.com/"
+  ? "https://voxmeet-0g91.onrender.com"
   : "http://localhost:8000";
 
 export default server;
