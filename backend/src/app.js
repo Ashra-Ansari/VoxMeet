265ac0e4e6289d6a54785e1f2ea65c213ce45ee5
Ashra Ansari <ashraansari02@gmail.com>
2026-09-21 14:25:56 +0530

    Ready for Deployment

---
+++ backend/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js ";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ extended: true, limit: "40kb" }));
+
+app.use("/api/v1/users", userRoutes);
+app.get("/", (req, res) => {
+  return res.json({ hello: "world" });
+});
+
+const start = async () => {
+  try {
+    const connectionDB = await mongoose.connect(
+      "mongodb+srv://ashra:ashra8228@cluster0.pyxag8b.mongodb.net/voxmeet?appName=Cluster0",
+    );
+    console.log(`MONGO Connected DB Host: ${connectionDB.connection.host}`);
+    server.listen(app.get("port"), () => {
+      console.log("listening on port 8000");
+    });
+  } catch (e) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+start();
